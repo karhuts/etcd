1a4efcb0ed9aad6e831825574297c60857cbaf06
Min <294953530@qq.com>
2023-08-29 21:11:37 +0800

    add

---
+++ src/Exception/EtcdException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace karthus\etcd;
+
+use Exception;
+
+class EtcdException extends Exception
+{
+}
---
+++ src/Exception/KeyExistsException.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+namespace karthus\etcd;
+
+class KeyExistsException extends EtcdException
+{
+}
